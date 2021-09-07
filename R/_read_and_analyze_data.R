2e16c03d3c3cf542b3d991e811311b5aabc45fe8
Кирилл ÑГÐулиев <guliev@ecopsy.ru>
2021-09-07 14:49:16 +0300

    1 task completed

--- R/_read_and_analyze_data.R
+++ R/_read_and_analyze_data.R
@@ -10,15 +10,43 @@ dem_data <- fread("./data/demography_data.csv")
 purchase <- fread("./data/purchase_data.tsv")
 
 joined_data <- purchase %>% 
-  inner_join(dem_data, by = "hhkey")
+  inner_join(dem_data, by = "hhkey") %>% 
+  mutate(movedate = as.character(movedate))
 
 # 1. Share of repeated purchases in the category ----
 # Group by date to find out the number of buyers in a current period of time
-# Also group by the buyer's cart ID which represents the purchase act
+# Also group by the buyer's ID and calculate the number of a distinct operation
 
 repeated_purchases <- joined_data %>% 
   group_by(movedate, hhkey) %>% 
-  summarise(repeated = n_distinct(occaskey),
-            share = repeated / sum(repeated)) %>% 
+  summarise(repeated = n_distinct(occaskey)) %>% 
   arrange(desc(repeated)) %>% 
+  ungroup() %>% 
+  mutate(share = round((repeated / sum(repeated)) * 100, 2))
+
+head(repeated_purchases, 5)
+
+# 2. Divide buyers by tertiles into 3 groups - heavy, medium, light ----
+# Compute tertiles among repeated buyers; find out the share of each group among the repeated buyers
+
+grouped <- repeated_purchases %>% 
+  filter(repeated >= 2) %>% 
+  select(hhkey) %>%
+  distinct() %>% 
+  inner_join(joined_data, by = "hhkey") %>% 
+  select(hhkey, occaskey, value, number) %>% 
+  group_by(hhkey) %>% 
+  summarise(cumulative_spending = sum(value)) %>% 
   ungroup()
+  # mutate(tertile = ntile(cumulative_spending, 3),
+  #        group = ifelse(tertile == 3, "heavy", ifelse(tertile == 2, "medium", "light")))
+
+tertiles <- quantile(grouped$cumulative_spending, c(0:3 / 3))
+
+grouped$group <- with(grouped,
+                       cut(cumulative_spending,
+                       tertiles,
+                       include.lowest = T,
+                       lables = c("heavy", "medium", "light")))
+
+levels(grouped$group) <- c("light", "medium", "heavy")
